9a6c96a945cdc1b33d0f0218ad0aa1ce04ed4cc1
sidahmedabdelillah <s_bentegar@enst.dz>
2023-02-21 15:51:43 +0100

    added notification

--- dist/api/yupa/yupa.types.d.ts
+++ dist/api/yupa/yupa.types.d.ts
@@ -2,4 +2,5 @@ export * from './dashboard/dashboard.types';
 export * from './payments/payments.types';
 export * from './tpos/tpos.types';
 export * from './wallet/wallet.types';
+export * from './notifications.types';
 //# sourceMappingURL=yupa.types.d.ts.map
---
+++ src/api/yupa/notifications.types.ts
@@ -0,0 +1,20 @@
+export type notification_status_enum = {
+  NEW: "NEW",
+  DELIVERED: "DELIVERED",
+  SEEN: "SEEN",
+  OPENED: "OPENED",
+  ARCHIVED: "ARCHIVED",
+};
+
+export type notification = {
+  id: number;
+  title: string | null;
+  info: string | null;
+  action_data: Record<string, any>;
+  account_id: string;
+  created_at: Date | null;
+  seen_at: Date | null;
+  opened_at: Date | null;
+  status: notification_status_enum | null;
+  type: string | null;
+};
