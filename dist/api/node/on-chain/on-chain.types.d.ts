c72407b90d9ba8a791cefd59da723fb7e3e3bacf
sidahmedabdelillah <s_bentegar@enst.dz>
2023-05-10 23:30:09 +0100

    added on chain node types

--- dist/api/node/on-chain/on-chain.types.d.ts
+++ dist/api/node/on-chain/on-chain.types.d.ts
@@ -49,4 +49,216 @@ export declare const CreateNodeOnChainAddresseSchema: z.ZodObject<{
     addresse: string;
 }>;
 export type CreateNodeOnChainAddresseType = z.infer<typeof CreateNodeOnChainAddresseSchema>;
+export declare const bitcoinTransactionSchema: z.ZodObject<{
+    id: z.ZodString;
+    is_confirmed: z.ZodBoolean;
+    tokens: z.ZodNumber;
+}, "strip", z.ZodTypeAny, {
+    id: string;
+    tokens: number;
+    is_confirmed: boolean;
+}, {
+    id: string;
+    tokens: number;
+    is_confirmed: boolean;
+}>;
+export type BitcoinTransactionType = z.infer<typeof bitcoinTransactionSchema>;
+export declare const sendToOnChainResponseSchema: z.ZodObject<{
+    transaction: z.ZodObject<{
+        id: z.ZodString;
+        is_confirmed: z.ZodBoolean;
+        tokens: z.ZodNumber;
+    }, "strip", z.ZodTypeAny, {
+        id: string;
+        tokens: number;
+        is_confirmed: boolean;
+    }, {
+        id: string;
+        tokens: number;
+        is_confirmed: boolean;
+    }>;
+}, "strip", z.ZodTypeAny, {
+    transaction: {
+        id: string;
+        tokens: number;
+        is_confirmed: boolean;
+    };
+}, {
+    transaction: {
+        id: string;
+        tokens: number;
+        is_confirmed: boolean;
+    };
+}>;
+export type sendToOnChainResponseType = z.infer<typeof sendToOnChainResponseSchema>;
+export declare const onChainTransactionSchema: z.ZodObject<{
+    block_id: z.ZodOptional<z.ZodString>;
+    confirmation_count: z.ZodOptional<z.ZodNumber>;
+    confirmation_height: z.ZodOptional<z.ZodNumber>;
+    created_at: z.ZodString;
+    description: z.ZodOptional<z.ZodString>;
+    fee: z.ZodOptional<z.ZodNumber>;
+    id: z.ZodString;
+    inputs: z.ZodArray<z.ZodObject<{
+        is_local: z.ZodBoolean;
+        transaction_id: z.ZodString;
+        transaction_vout: z.ZodNumber;
+    }, "strip", z.ZodTypeAny, {
+        transaction_id: string;
+        transaction_vout: number;
+        is_local: boolean;
+    }, {
+        transaction_id: string;
+        transaction_vout: number;
+        is_local: boolean;
+    }>, "many">;
+    is_confirmed: z.ZodBoolean;
+    is_outgoing: z.ZodBoolean;
+    output_addresses: z.ZodArray<z.ZodString, "many">;
+    tokens: z.ZodNumber;
+    transaction: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    fee?: number | undefined;
+    description?: string | undefined;
+    transaction?: string | undefined;
+    block_id?: string | undefined;
+    confirmation_count?: number | undefined;
+    confirmation_height?: number | undefined;
+    id: string;
+    created_at: string;
+    tokens: number;
+    is_confirmed: boolean;
+    inputs: {
+        transaction_id: string;
+        transaction_vout: number;
+        is_local: boolean;
+    }[];
+    is_outgoing: boolean;
+    output_addresses: string[];
+}, {
+    fee?: number | undefined;
+    description?: string | undefined;
+    transaction?: string | undefined;
+    block_id?: string | undefined;
+    confirmation_count?: number | undefined;
+    confirmation_height?: number | undefined;
+    id: string;
+    created_at: string;
+    tokens: number;
+    is_confirmed: boolean;
+    inputs: {
+        transaction_id: string;
+        transaction_vout: number;
+        is_local: boolean;
+    }[];
+    is_outgoing: boolean;
+    output_addresses: string[];
+}>;
+export type OnChainTransactionsype = z.infer<typeof onChainTransactionSchema>;
+export declare const getOnChainTransactionsResponseSchema: z.ZodObject<{
+    transactions: z.ZodArray<z.ZodObject<{
+        block_id: z.ZodOptional<z.ZodString>;
+        confirmation_count: z.ZodOptional<z.ZodNumber>;
+        confirmation_height: z.ZodOptional<z.ZodNumber>;
+        created_at: z.ZodString;
+        description: z.ZodOptional<z.ZodString>;
+        fee: z.ZodOptional<z.ZodNumber>;
+        id: z.ZodString;
+        inputs: z.ZodArray<z.ZodObject<{
+            is_local: z.ZodBoolean;
+            transaction_id: z.ZodString;
+            transaction_vout: z.ZodNumber;
+        }, "strip", z.ZodTypeAny, {
+            transaction_id: string;
+            transaction_vout: number;
+            is_local: boolean;
+        }, {
+            transaction_id: string;
+            transaction_vout: number;
+            is_local: boolean;
+        }>, "many">;
+        is_confirmed: z.ZodBoolean;
+        is_outgoing: z.ZodBoolean;
+        output_addresses: z.ZodArray<z.ZodString, "many">;
+        tokens: z.ZodNumber;
+        transaction: z.ZodOptional<z.ZodString>;
+    }, "strip", z.ZodTypeAny, {
+        fee?: number | undefined;
+        description?: string | undefined;
+        transaction?: string | undefined;
+        block_id?: string | undefined;
+        confirmation_count?: number | undefined;
+        confirmation_height?: number | undefined;
+        id: string;
+        created_at: string;
+        tokens: number;
+        is_confirmed: boolean;
+        inputs: {
+            transaction_id: string;
+            transaction_vout: number;
+            is_local: boolean;
+        }[];
+        is_outgoing: boolean;
+        output_addresses: string[];
+    }, {
+        fee?: number | undefined;
+        description?: string | undefined;
+        transaction?: string | undefined;
+        block_id?: string | undefined;
+        confirmation_count?: number | undefined;
+        confirmation_height?: number | undefined;
+        id: string;
+        created_at: string;
+        tokens: number;
+        is_confirmed: boolean;
+        inputs: {
+            transaction_id: string;
+            transaction_vout: number;
+            is_local: boolean;
+        }[];
+        is_outgoing: boolean;
+        output_addresses: string[];
+    }>, "many">;
+}, "strip", z.ZodTypeAny, {
+    transactions: {
+        fee?: number | undefined;
+        description?: string | undefined;
+        transaction?: string | undefined;
+        block_id?: string | undefined;
+        confirmation_count?: number | undefined;
+        confirmation_height?: number | undefined;
+        id: string;
+        created_at: string;
+        tokens: number;
+        is_confirmed: boolean;
+        inputs: {
+            transaction_id: string;
+            transaction_vout: number;
+            is_local: boolean;
+        }[];
+        is_outgoing: boolean;
+        output_addresses: string[];
+    }[];
+}, {
+    transactions: {
+        fee?: number | undefined;
+        description?: string | undefined;
+        transaction?: string | undefined;
+        block_id?: string | undefined;
+        confirmation_count?: number | undefined;
+        confirmation_height?: number | undefined;
+        id: string;
+        created_at: string;
+        tokens: number;
+        is_confirmed: boolean;
+        inputs: {
+            transaction_id: string;
+            transaction_vout: number;
+            is_local: boolean;
+        }[];
+        is_outgoing: boolean;
+        output_addresses: string[];
+    }[];
+}>;
+export type GetOnChainTransactionsResponseType = z.infer<typeof getOnChainTransactionsResponseSchema>;
 //# sourceMappingURL=on-chain.types.d.ts.map
--- src/api/node/on-chain/on-chain.types.ts
+++ src/api/node/on-chain/on-chain.types.ts
@@ -6,17 +6,68 @@ export const NodeOnChainAddresseSchema = z.object({
   tokens: z.number(),
 })
 
-export type  NodeOnChainAddresseType = z.infer<typeof NodeOnChainAddresseSchema>
+export type NodeOnChainAddresseType = z.infer<typeof NodeOnChainAddresseSchema>
 
-export const GetNodeOnChainAddresseSchema = z.object({ 
-    addresses: z.array(NodeOnChainAddresseSchema)
+export const GetNodeOnChainAddresseSchema = z.object({
+  addresses: z.array(NodeOnChainAddresseSchema),
 })
 
-export type GetNodeOnChainAddresseType = z.infer<typeof GetNodeOnChainAddresseSchema>
-
+export type GetNodeOnChainAddresseType = z.infer<
+  typeof GetNodeOnChainAddresseSchema
+>
 
 export const CreateNodeOnChainAddresseSchema = z.object({
-    addresse: z.string()
+  addresse: z.string(),
 })
 
-export type CreateNodeOnChainAddresseType = z.infer<typeof CreateNodeOnChainAddresseSchema>
+export type CreateNodeOnChainAddresseType = z.infer<
+  typeof CreateNodeOnChainAddresseSchema
+>
+
+export const bitcoinTransactionSchema = z.object({
+  id: z.string(),
+  is_confirmed: z.boolean(),
+  tokens: z.number(),
+})
+
+export type BitcoinTransactionType = z.infer<typeof bitcoinTransactionSchema>
+
+export const sendToOnChainResponseSchema = z.object({
+  transaction: bitcoinTransactionSchema,
+})
+
+export type sendToOnChainResponseType = z.infer<
+  typeof sendToOnChainResponseSchema
+>
+
+export const onChainTransactionSchema = z.object({
+  block_id: z.string().optional(),
+  confirmation_count: z.number().optional(),
+  confirmation_height: z.number().optional(),
+  created_at: z.string(),
+  description: z.string().optional(),
+  fee: z.number().optional(),
+  id: z.string(),
+  inputs: z.array(
+    z.object({
+      is_local: z.boolean(),
+      transaction_id: z.string(),
+      transaction_vout: z.number(),
+    })
+  ),
+  is_confirmed: z.boolean(),
+  is_outgoing: z.boolean(),
+  output_addresses: z.array(z.string()),
+  tokens: z.number(),
+  transaction: z.string().optional(),
+})
+
+export type OnChainTransactionsype = z.infer<typeof onChainTransactionSchema>
+
+export const getOnChainTransactionsResponseSchema = z.object({
+  transactions: z.array(onChainTransactionSchema),
+})
+
+export type GetOnChainTransactionsResponseType = z.infer<
+  typeof getOnChainTransactionsResponseSchema
+>
